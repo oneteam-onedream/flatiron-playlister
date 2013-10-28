5e01e0ee757d1b1c5ab47f148b0dd8658a0f0302
David Bella <dbella1@gmail.com>
2013-10-28 13:15:31 -0400

    Updating blob

--- lib/example_support.rb
+++ lib/example_support.rb
@@ -39,4 +39,4 @@ end
 # Load the configuration.
 $appkey = IO.read("lib/spotify_appkey.key", encoding: "BINARY")
 $username = "davidbella"
-$blob = "GT75p9eLEK6WPTOBEc3m4VVQROR14tAYG4tiXJPnx24/81GXb3ZXrSJg88OJa8Ra"
+$blob = "GT75p9eLEK6WPTOBEc3m4VVQROR14tAYG4tiXJPnx26IQxRpFcjzrt8qp2EsQJUY"
