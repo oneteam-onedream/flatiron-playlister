17657a6aa3d9a0501d1fd0eee84d9a32200ef98b
Kyle Shike <kyleshike@gmail.com>
2013-10-31 06:38:46 -0400

    playlist controller

---
+++ app/controllers/playlist_controller.rb
@@ -0,0 +1,58 @@
+class PlaylistController < ApplicationController
+  enable :sessions
+
+  get '/' do
+    redirect '/playlist'
+  end
+
+  get '/spotify' do
+    @query = Spotify_Finder.search(params[:search])
+    session[:query] = @query
+    redirect '/playlist'
+  end
+
+  post '/playlist/add' do
+    Playlist[1].add_song(params[:song])
+    session[:query] = nil
+    redirect '/playlist'
+  end
+
+  get '/playlist' do 
+    @query = session[:query]
+    @playlist = Playlist[1]
+    @songs = @playlist.songs
+    erb :'playlist'
+  end
+
+  get '/songs/:slug/upvote' do
+    @song = Playlist[1].songs.detect {|song| song.slug == params[:slug]}
+    @song.upvote(request.ip)
+    redirect '/playlist'
+  end
+
+end
+
+# HEROKU OR RASPBERRY PI
+
+# master page
+#   -master user creates 'room' with name and password
+#   -teather to spotify login
+#   -master streams songs
+#   -has user page functionality
+
+# user page
+
+
+
+# duplication!
+# Spotify functionality
+# AJAX
+#   -search results
+#   -list sorting by vote count
+#   -
+# Where to play the song?
+#   -adding to database with after_play callback
+#   -removing song from list with after_play callback
+# CSS/ page design
+# archive/ history page?
+# server/ database
